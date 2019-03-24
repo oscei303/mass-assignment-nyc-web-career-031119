9008ab8177b36bc6178757ae6b837bb6b3881c99
OSCAR R <oscr.rss@gmail.com>
2019-03-24 18:22:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
+
 class Person
-  #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attribute)
+    @name = attribute[:name]
+    @birthday = attribute[:birthday]
+    @hair_color = attribute[:hair_color]
+    @eye_color = attribute[:eye_color] 
+    @height = attribute[:height] 
+    @weight = attribute[:weight]
+    @handed = attribute[:handed]
+    @complexion = attribute[:complexion]
+    @t_shirt_size = attribute[:t_shirt_size] 
+    @wrist_size = attribute[:wrist_size]
+    @glove_size = attribute[:glove_size] 
+    @pant_length = attribute[:pant_length] 
+    @pant_width = attribute[:pant_width]
+  end
+
 end
